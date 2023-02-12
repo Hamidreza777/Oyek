8aa5d604d332d21e454e39b8c76aa7208f677522
artin <m.malekii2003@gmail.com>
2023-02-12 17:51:50 +0330

    finish

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Heder from './Component/Heder/Heder';
+import Body from './Component/Body/Body';
+import Footer from './Component/Footer/Footer';
+
+const App=()=>{
+return(
+    <>
+    <Heder/>
+    <Body/>
+    <Footer/>
+    </>
+)
+
+
+
+
+}
+export default App;
---
+++ src/Component/Body/Body.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Body=()=>{
+return(
+    <>
+<h2>hello</h2>
+    </>
+)
+
+
+
+}
+export default Body;
