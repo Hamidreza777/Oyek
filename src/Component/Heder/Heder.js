3865bb86d4fc5c0e55f6a6eb6dc497b67bf0fb58
artin <m.malekii2003@gmail.com>
2023-02-12 19:27:33 +0330

    first

--- src/Component/Heder/Heder.js
+++ src/Component/Heder/Heder.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Hedercss from "../Heder/Heder.module.css"
+import logo from "../../pic/logo.png"
 
 const Heder=()=>{
 return(
 <>
-<h1>in the name of god</h1>
+<header>
+  <nav>
+  
+  <ul>
+  <li><img className={Hedercss.logo} src={logo}></img></li>
+
+    <li><a>خانه</a> </li>
+    <li><a>خدمات ما </a></li>
+    <li><a>مقاله ها</a></li>
+    <li><a>تماس  با ما</a></li>
+
+   
+  </ul>
+
+
+  </nav>
+
+
+
+   
+    
+</header>
 
 </>
 
