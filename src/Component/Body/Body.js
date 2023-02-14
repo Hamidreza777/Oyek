75d6286fb2347424a4c07832948eddb606d8405d
benyamin <benyaminzbz84@gmail.com>
2023-02-14 15:34:41 +0330

    box

--- src/Component/Body/Body.js
+++ src/Component/Body/Body.js
@@ -7,9 +7,7 @@ const Body=()=>{
 return(
    <div>
    <div className={bodycss.contaner}>
-      <div className={bodycss.image1}>
-           <img src={man} alt=""></img>
-      </div>
+     
        <div className={bodycss.h}>
          <h1>به اُیک خوش آمدید</h1>
        
@@ -20,12 +18,21 @@ return(
 برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
 
          </p>
+     
          </div>
+         <div className={bodycss.image1}>
+           <img src={man} alt=""></img>
+      </div>
+         <h2>بررسی خدمات ما</h2>
          </div>
-   
-   
+         <center>
+         <div className={bodycss.khadamat}>
 
+         </div>
+         </center>
+       
    </div>
+   
    
 )
 
