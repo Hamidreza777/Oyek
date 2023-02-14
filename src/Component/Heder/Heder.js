edd175dca6cac2a4b3591054d142b577b0f8ba38
artin <m.malekii2003@gmail.com>
2023-02-14 11:12:44 +0330

    header

--- src/Component/Heder/Heder.js
+++ src/Component/Heder/Heder.js
@@ -1,38 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Hedercss from "../Heder/Heder.module.css"
+import font from "../../font/Yekan.ttf"
 import logo from "../../pic/logo.png"
+
 
 const Heder=()=>{
 return(
 <>
 <header>
-  <nav>
-  
-  <ul>
-  <li><img className={Hedercss.logo} src={logo}></img></li>
+<nav>
 
-    <li><a>خانه</a> </li>
-    <li><a>خدمات ما </a></li>
-    <li><a>مقاله ها</a></li>
-    <li><a>تماس  با ما</a></li>
-    <a>sdkf</a>
-
-   
-  </ul>
+<img className={Hedercss.logo} src={logo}></img>
+<ul>
+<li><a>خانه</a></li>
+<li><a>خدمات ما</a></li>
+<li><a>مقاله ها</a></li>
+<li><a>تماس با ما</a></li>
 
 
-  </nav>
+</ul>
+<b className={Hedercss.call}>تماس:09920136476 </b>
+</nav>
 
 
-
-   
-    
 </header>
-
 </>
 
 )
+
+
+
 
 }
 export default Heder;
