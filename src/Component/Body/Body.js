5b609f7848486395dae480432e6fa21e250a5cfc
benyamin <benyaminzbz84@gmail.com>
2023-02-15 13:56:32 +0330

    finish-topbody

--- src/Component/Body/Body.js
+++ src/Component/Body/Body.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import bodycss from "../Body/Body.module.css"
 import man from "../../pic/Final photo.png"
-
+import web from '../../pic/Web design.png'
+import moh from "../../pic/mohtava.png"
+import ux from "../../pic/ux ui.png"
+import seo from "../../pic/seo.png"
 const Body=()=>{
 return(
    <div>
+      <div className={bodycss.div1}>
    <div className={bodycss.contaner}>
      
        <div className={bodycss.h}>
@@ -14,23 +18,64 @@ return(
       
          <p>
          برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-
-برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-
          </p>
-     
+         <p>
+برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+</p>
+         
          </div>
          <div className={bodycss.image1}>
            <img src={man} alt=""></img>
       </div>
          <h2>بررسی خدمات ما</h2>
-         </div>
+
+
          <center>
          <div className={bodycss.khadamat}>
+
+            <div className={bodycss.tarahi}>
+               <br></br>
+               <br></br>
+               <img src={web} ></img>
+               <br></br>
+               <br></br>
+               <h3>طراحی سایت</h3>
+            </div>
+
+            <div className={bodycss.ux }>
+            <br></br>
+            <br></br>
+               <img src={ux} ></img>
+               <br></br>
+               <br></br>
+               <h3>ّUX/UI</h3>
+            </div>
+
+            <div className={bodycss.seo}>
+            <br></br>
+            <br></br>
+               <img src={seo}></img>
+               <br></br>
+               <br></br>
+               <h3>SEO</h3>
+            </div>
+
+            <div className={bodycss.tolid}>
+            <br></br>
+            <br></br>
+               <img src={moh}></img>
+               <br></br>
+               <br></br>
+               <h4>تولید محتوا</h4>
+            </div>
          </div>
-         </center>
-         <p>fflkdfkf</p>
-       
+         
+        </center>
+         </div>
+        
+
+         
+         </div>
    </div>
    
    
