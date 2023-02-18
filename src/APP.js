3f7532651e45fbf7554fa5f85362e0cd6bd97aaa
hamidreza soltani <h.soltani8452@gmail.com>
2023-02-18 13:30:45 +0330

    rm body

--- src/APP.js
+++ src/APP.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';  
-import Heder from './Component/Heder/Heder';
+import React from "react";
+import ReactDOM from "react-dom";
+import Heder from "./Component/Heder/Heder";
 
-import Body  from './Component/Body/Body';
-const APP=()=>{
-return(
-<>
-<Heder/>
-<Body/>
-</>
-)
-
-
-}
+import Body from "./Component/Body/Body";
+const APP = () => {
+  return (
+    <>
+      <Heder /> <Body />
+    </>
+  );
+};
 export default APP;
--- src/Component/Body/Body.js
+++ src/Component/Body/Body.js
@@ -1,113 +1,108 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import bodycss from "../Body/Body.module.css"
-import man from "../../pic/Final photo.png"
-import web from '../../pic/Web design.png'
-import moh from "../../pic/mohtava.png"
-import ux from "../../pic/ux ui.png"
-import seo from "../../pic/seo.png"
+import React from "react";
+import bodycss from "../Body/Body.module.css";
+import man from "../../pic/Final photo.png";
+import web from "../../pic/Web design.png";
+import moh from "../../pic/mohtava.png";
+import ux from "../../pic/ux ui.png";
+import seo from "../../pic/seo.png";
 
-import abutus from "../../pic/about us.png"
-const Body=()=>{
-return(
-   <div>
+import abutus from "../../pic/about us.png";
+const Body = () => {
+  return (
+<></>
+
+
+/*     <div>
       <div className={bodycss.div1}>
-   <div className={bodycss.contaner}>
-     
-       <div className={bodycss.h}>
-         <h1>به اُیک خوش آمدید</h1>
-       
-      
-         <p>
-         برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-         </p>
-         <p>
-برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-</p>
-         
-         </div>
-         <div className={bodycss.image1}>
-           <img src={man} alt=""></img>
+        <div className={bodycss.contaner}>
+          <div className={bodycss.h}>
+            <h1>به اُیک خوش آمدید</h1>
+
+            <p>
+              برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن
+              ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+              گرافیک است.برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم
+              ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+              از طراحان گرافیک است.
+            </p>
+            <p>
+              برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن
+              ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+              گرافیک است.
+            </p>
+          </div>
+          <div className={bodycss.image1}>
+            <img src={man} alt=""></img>
+          </div>
+          <h2>بررسی خدمات ما</h2>
+
+          <center>
+            <div className={bodycss.khadamat}>
+              <div className={bodycss.tarahi}>
+                <br></br>
+                <br></br>
+                <img src={web}></img>
+                <br></br>
+                <br></br>
+                <h3>طراحی سایت</h3>
+              </div>
+
+              <div className={bodycss.ux}>
+                <br></br>
+                <br></br>
+                <img src={ux}></img>
+                <br></br>
+                <br></br>
+                <h3>ّUX/UI</h3>
+              </div>
+
+              <div className={bodycss.seo}>
+                <br></br>
+                <br></br>
+                <img src={seo}></img>
+                <br></br>
+                <br></br>
+                <h3>SEO</h3>
+              </div>
+
+              <div className={bodycss.tolid}>
+                <br></br>
+                <br></br>
+                <img src={moh}></img>
+                <br></br>
+                <br></br>
+                <h4>تولید محتوا</h4>
+              </div>
+            </div>
+          </center>
+        </div>
+
+        <div className={bodycss.baner}>
+          <div className={bodycss.text}>
+            <h2>که هستیم؟</h2>
+            <p>
+              برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن
+              ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+              گرافیک است.برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم
+              ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+              از طراحان گرافیک است.
+              <br></br>
+              <br></br>
+              برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن
+              ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
+              گرافیک است.برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم
+              ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+              از طراحان گرافیک است.
+            </p>
+          </div>
+
+          <div className={bodycss.aks}>
+            <img src={abutus}></img>
+          </div>
+        </div>
       </div>
-         <h2>بررسی خدمات ما</h2>
-
-
-         <center>
-         <div className={bodycss.khadamat}>
-
-            <div className={bodycss.tarahi}>
-               <br></br>
-               <br></br>
-               <img src={web} ></img>
-               <br></br>
-               <br></br>
-               <h3>طراحی سایت</h3>
-            </div>
-
-            <div className={bodycss.ux }>
-            <br></br>
-            <br></br>
-               <img src={ux} ></img>
-               <br></br>
-               <br></br>
-               <h3>ّUX/UI</h3>
-            </div>
-
-            <div className={bodycss.seo}>
-            <br></br>
-            <br></br>
-               <img src={seo}></img>
-               <br></br>
-               <br></br>
-               <h3>SEO</h3>
-            </div>
-
-            <div className={bodycss.tolid}>
-            <br></br>
-            <br></br>
-               <img src={moh}></img>
-               <br></br>
-               <br></br>
-               <h4>تولید محتوا</h4>
-            </div>
-         </div>
-         
-        </center>
-         </div>
-        
-
-         <div className={bodycss.baner}>
-    
-    <div className={bodycss.text}>
-<h2>
-که هستیم؟
-</h2>
-<p>
-
-
-برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-<br></br><br></br>
-برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
-
-</p>
-
-
-
-    </div>
-    
-    <div className={bodycss.aks}>
-      <img src={abutus}></img>
-    </div>
-  </div>
-         
-         </div>
-   </div>
-   
-   
-)
-
-
-
-}
+    </div> */
+  );
+};
 
 export default Body;
