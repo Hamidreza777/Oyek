b4743e839159e44e58a5376a26130b89762e7778
artin <m.malekii2003@gmail.com>
2023-02-28 13:34:47 +0330

    Footer

--- src/APP.js
+++ src/APP.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Heder from "./Component/Heder/Heder";
+import Footer from "./Component/Footer/Footer";
 
 import Body from "./Component/Body/Body";
 const APP = () => {
   return (
     <>
-      <Heder /> <Body />
+      <Heder /> <Body /><Footer/>
     </>
   );
 };
--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,11 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Fotercss from '../Footer/Footer.module.css'
+import logo from '../../pic/logo.png'
+import { BsInstagram } from "react-icons/bs";
+import { CiMail } from "react-icons/ci";
+import { AiOutlineGithub } from "react-icons/ai";
+import { ImTelegram } from "react-icons/im";
+
 
 
 const Footer=()=>{
 return(
     <>
-    
+ <footer>
+ <div className={Fotercss.logo}>
+<img src={logo}></img>
+
+</div>
+<div className={Fotercss.text}>
+<h3>درباره ما</h3>
+<p>برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+
+برای تغییر این متن بر روی دکمه ویرایش کلیک کنید. لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+
+
+</p>
+
+</div>
+
+<div className={Fotercss.shabakeh}>
+
+<BsInstagram className={Fotercss.insta} />
+
+<CiMail className={Fotercss.gmail} />
+
+<AiOutlineGithub className={Fotercss.git} />
+
+<ImTelegram/>
+</div>
+
+
+
+
+ </footer>
     </>
 )}
 export default Footer;
